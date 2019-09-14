8e2dea3a52b49d1eab9a6601f58d693fae4ed351
eyobs <ttoeyob@gmail.com>
2019-09-14 16:41:54 +0300

    16

--- overthewire/Natas/natas13f/exif.php
+++ overthewire/Natas/natas13f/exif.php
@@ -1,4 +1,12 @@
 <?php
+    // phpinfo();
+
+    // if (function_exists('exif_imagetype')) {
+    //     echo "This function is installed";
+    // } else {
+    //     echo "It is not";
+    // }
+
     $d=exif_imagetype('img.jpg');
-    echo '$d';
+    echo $d;
 ?>
