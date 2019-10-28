4260a63bca9a15d51673097dc66f7dbfcbc57d8d
eyobs <ttoeyob@gmail.com>
2019-10-28 08:27:32 +0300

    25

---
+++ overthewire/Natas/natas23f/test.php
@@ -0,0 +1,9 @@
+<?php
+
+if(strstr("90hhh iloveyou","iloveyou") && ("90hhh iloveyou" > 10 )){
+    echo "<br>The credentials for the next level are:<br>";
+}
+
+// echo ("90hhh iloveyou" > 10 )
+        
+?>  
---
+++ overthewire/Natas/natas24f/test.php
@@ -0,0 +1,7 @@
+<?php
+        // if(!strcmp("c","a")){
+        //     echo "<br>The credentials for the next level are:<br>";
+        //     echo "<pre>Username: natas25 Password: <censored></pre>";
+        // }
+        echo strcmp("aa","}");
+?>
